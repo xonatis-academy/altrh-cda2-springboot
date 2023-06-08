a6cec96ba8608e9e22377591775546ba9ba0b22a
Super Stream <super-stream@xonatis.io>
2023-06-08 11:31:03 +0200

    Adding listing

---
+++ stocky/src/main/java/fr/prody/prody/ProdyApplication.java
@@ -0,0 +1,13 @@
+package fr.prody.prody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdyApplication.class, args);
+	}
+
+}
---
+++ stocky/src/test/java/fr/prody/prody/ProdyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.prody.prody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
